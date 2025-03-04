55cebf6829942131a27bf036e847e82fb8ea45dc
ibrahim saber <ibrahimsaber622@gmail.com>
2025-03-04 20:43:32 +0200

    add : Implement search function in 0033_Search_in_Rotated_Sorted_Array.py and add 0153_Find_Minimum_in_Rotated_Sorted_Array.py

--- python/0033_Search_in_Rotated_Sorted_Array.py
+++ python/0033_Search_in_Rotated_Sorted_Array.py
@@ -57,5 +57,13 @@ class Solution(object):
 
         return -1
 
+# ----------
+class Solution(object):
+    def search(self, nums, target):
+        for i in range(len(nums)):
+            if nums[i] == target:
+                return i
+        return -1
+
 
 #--------------------------------------------------------------------------------------------------------------------------------------
