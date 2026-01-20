0a3442a2058fe9acb73bf1f0c350e540838c7cca
Ibrahim Saber <ibrahimsaber622@gmail.com>
2026-01-20 21:29:28 +0200

    add: Container With Most Water

---
+++ python/11_Container_With_Most_Water.py
@@ -0,0 +1,61 @@
+# 11. Container With Most Water
+
+# You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+# Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+# Return the maximum amount of water a container can store.
+
+# Notice that you may not slant the container.
+
+#  Input: height = [1,8,6,2,5,4,8,3,7]
+# Output: 49
+# Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+# Example 2:
+
+# Input: height = [1,1]
+# Output: 1
+
+# ------------------------------------------------------ ------------------------------------------------------ ------------------------------------------------------
+# ------------------------------------------------------ ---------------------- solution ---------------------- ------------------------------------------------------
+# ------------------------------------------------------ ------------------------------------------------------ ------------------------------------------------------
+# this is the basic solution but is exceeded the time limit
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        maxArea=0
+        for i in range(len(height)):
+            for j in range(i+1 ,len(height)):
+                maxArea = max(maxArea, self.get_area(height, i, j ))
+
+        return maxArea
+
+
+    def get_area(self, height, i, j):
+        return min(height[i], height[j]) * (j-i)
+
+
+# ------------------------------------------------------ ------------------------------------------------------ ------------------------------------------------------
+
+# this is solution using two pointers
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) - 1
+        maxArea = 0
+
+        while left < right:
+            currentArea = min(height[left], height[right]) * (right - left)
+            maxArea = max(maxArea, currentArea)
+
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return maxArea
+
+
